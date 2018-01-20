71ba84c64a23acd887c606d0130d7c158d902fea
Jürgen Mülbert <develop@juergen-muelbert.de>
2018-01-20 10:56:42 +0100

    First version.

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # homebrew-qtifw
+
 The QT Installer Framework for Homebrew
+
+** Install
+
+#+BEGIN_SRC bash
+$ brew tap jmuelbert/homebrew-qtifw
+$ brew install qt-ifw [options]
+#+END_SRC
+
+By default (without any additional options) this formula install the
+Qt Installer Framework. Please see the list of available options to
+disable any bit of default behaiviour or add even more stuff.
+
+** Options
+
+To get list of all available options, call ~brew info qt-ifw~. Here is
+the relevant part from output that command for your covenience.
+
+#+BEGIN_SRC bash
+==> Options
+#+END_SRC
+
+Note that by default =qt-ifw= builds all Commandline tools are installed.
+
+Note that =--HEAD= builds are not really supported. They may fail to install,
+they may crash at run time. Or even worse, the may work as expected. So use this
+option at your own risk. And feel free to open up issues.
+
+** Known Issues
+
+Please checkout [[https://github.com/jmuelbert/homebrew-qtifw/issues][Issues]] page for a list of all known issues.
+
+** Acknowledgements
+
+Many thanks to everyone reporting issues.s
---
+++ qt-ifw.rb
@@ -0,0 +1,59 @@
+
+class QtIfw < Formula
+  desc "The Qt Installer Framework"
+  homepage "http://doc.qt.io/qtinstallerframework/"
+  url "http://download.qt.io/official_releases/qt-installer-framework/3.0.2/qt-installer-framework-opensource-src-3.0.2.tar.gz"
+  sha256 "f65059141278e0a1224a5b821ae8affe59ee4bb4b50abdc4e04f93fd69a02617"
+  head="http://code.qt.io/cgit/installer-framework/installer-framework.git/"
+
+  depends_on "qt"
+
+  def install
+
+    cd "src/libs/7zip" do
+      inreplace "7zip.pro" do |s|
+        s.gsub! "$$[QT_INSTALL_LIBS]", lib
+      end
+    end
+
+    cd "src/libs/installer" do
+      inreplace "installer.pro" do |s|
+        s.gsub! "$$[QT_INSTALL_LIBS]", lib
+      end
+    end
+
+    cd "tools/archivegen" do
+      inreplace "archivegen.pro" do |s|
+        s.gsub! "$$[QT_INSTALL_BINS]", bin
+      end
+    end
+
+    cd "tools/binarycreator" do
+      inreplace "binarycreator.pro" do |s|
+        s.gsub! "$$[QT_INSTALL_BINS]", bin
+      end
+    end
+
+    cd "tools/repogen" do
+      inreplace "repogen.pro" do |s|
+        s.gsub! "$$[QT_INSTALL_BINS]", bin
+      end
+    end
+
+    cd "tools/devtool" do
+      inreplace "devtool.pro" do |s|
+        s.gsub! "$$[QT_INSTALL_BINS]", bin
+      end
+    end
+
+    system "qmake", "installerfw.pro", "-r", "-spec", "macx-clang",
+                                              "-config", "release",
+                                              "PREFIX=#{prefix}"
+
+    system "make", "install"
+  end
+
+  test do
+    assert shell_output("#{bin}/binarycreator --h")
+  end  
+end
