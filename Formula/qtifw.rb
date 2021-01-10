d43572307bf39784d9b0cd86d3387b168ef5fc50
Jürgen <juergen.muelbert@gmail.com>
2021-01-10 10:14:03 +0100

    Resolve filename error.

--- Formula/qtifw.rb
+++ Formula/qtifw.rb
@@ -8,7 +8,7 @@ class Qtifw < Formula
   homepage 'http://doc.qt.io/qtinstallerframework/'
   version '4.0.1x'
   sha256 'ef04180c2779b781754fb198ad55b190c199baa27ee18ba09970959a6a6bdc22'
-  url 'http://download.qt.io/official_releases/qt-installer-framework/4.0.1/qt-installer-framework-4.01.tar'
+  url 'http://download.qt.io/official_releases/qt-installer-framework/4.0.1/qt-installer-framework-4.0.1.tar'
   head 'http://code.qt.io/cgit/installer-framework/installer-framework.git/'
 
   depends_on 'qt' => '5.12'
