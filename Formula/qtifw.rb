ec8f7d4007b0971bbe1d2c10622f7d11779d9abf
Jürgen Mülbert <jmuelbert@users.noreply.github.com>
2023-03-19 11:53:56 +0100

    fix: security issues on the formula

--- Formula/qtifw.rb
+++ Formula/qtifw.rb
@@ -10,11 +10,11 @@
 # with homebrew
 class Qtifw < Formula
   desc 'The Qt Installer Framework'
-  homepage 'http://doc.qt.io/qtinstallerframework/'
+  homepage 'https://doc.qt.io/qtinstallerframework/'
   version '4.5.1'
   sha256 '602417a0a2ada5cada8f5b6ad5a160390198b68b46a3721022b7370a971b040a'
   url 'https://download.qt.io/official_releases/qt-installer-framework/4.5.1/installer-framework-opensource-src-4.5.1.tar.xz'
-  head 'http://code.qt.io/cgit/installer-framework/installer-framework.git/'
+  head 'https://code.qt.io/cgit/installer-framework/installer-framework.git/'
 
   depends_on 'qt@5'
   depends_on 'xz' => :build
