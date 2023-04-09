6bfe301d4d5fb8997e0a55fb4283006e9c867352
Jürgen <juergen.muelbert@gmail.com>
2023-04-09 11:47:25 +0200

    Update consts on pages

--- docs/src/consts.ts
+++ docs/src/consts.ts
@@ -22,6 +22,7 @@ export const KNOWN_LANGUAGE_CODES = Object.values(KNOWN_LANGUAGES);
 export const GITHUB_EDIT_URL = `https://github.com/jmuelbert/homebrew-qtifw/tree/main/docs`;
 
 // export const COMMUNITY_INVITE_URL = `https://astro.build/chat`;
+export const COMMUNITY_INVITE_URL = ''
 
 // See "Algolia" section of the README for more information.
 export const ALGOLIA = {
