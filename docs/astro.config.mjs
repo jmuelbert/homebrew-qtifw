0fdac5b95eab1f10056b39cbd53a0153d3e85a28
Jürgen <juergen.muelbert@gmail.com>
2023-04-10 10:19:46 +0200

    Update documentation

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,15 +1,14 @@
-import { defineConfig } from 'astro/config';
-import preact from '@astrojs/preact';
-import react from '@astrojs/react';
+import { defineConfig } from 'astro/config'
 
-// https://astro.build/config
+import mdx from '@astrojs/mdx'
+import preact from '@astrojs/preact'
+import react from '@astrojs/react'
+import sitemap from '@astrojs/sitemap'
+
+import minifier from 'astro-html-minifier'
+
 export default defineConfig({
-  integrations: [
-    // Enable Preact to support Preact JSX components.
-    preact(),
-    // Enable React for the Algolia search component.
-    react(),
-  ],
-  site: `https://jmuelbert.github.io`,
-  base: '/homebrew-qtifw',
-});
+  integrations: [mdx(), minifier(), preact(), react(), sitemap()],
+  base: `/homebrew-qtifw`,
+  site: `https://jmuelbert.github.io`
+})
