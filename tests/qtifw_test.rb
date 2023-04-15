cdbe15fc77ce301de031082b89e4b7bf827f32ae
Jürgen Mülbert <juergen.muelbert@gmail.com>
2023-04-15 08:27:47 +0000

    Resolve isses on tests

--- tests/qtifw_test.rb
+++ tests/qtifw_test.rb
@@ -1,14 +1,24 @@
+#
+# SPDX-FileCopyrightText: 2021 Jürgen Mülbert <juergen.muelbert@gmail.com>
+#
+# SPDX-License-Identifier: EUPL-1.2
+#
+# frozen_string_literal: true
+
 require 'test/unit'
 require_relative '../Formula/qtifw'
 
+# Tests for Class Qtifw to install
+# the QT Installer Framework
+# with homebrew
 class QtifwTests < Test::Unit::TestCase
     
     Test::Unit.at_start do
-      puts 'START: runs only once at start'
+        puts 'START: runs only once at start'
     end
 
     Test::Unit.at_exit do
-      puts 'END: runs only once at end'
+        puts 'END: runs only once at end'
     end
 
     def setup
@@ -20,7 +30,6 @@ class QtifwTests < Test::Unit::TestCase
     end    
 
     def test_default()
-
         assert_equal(2,2)
     end
 end
