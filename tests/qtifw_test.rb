05f32e42856768e5d9c5f8c0353fe7eb5ed7afad
Jürgen Mülbert <juergen.muelbert@gmail.com>
2023-04-15 08:33:30 +0000

    Update qtifw_test.rb

--- tests/qtifw_test.rb
+++ tests/qtifw_test.rb
@@ -13,23 +13,23 @@ require_relative '../Formula/qtifw'
 # with homebrew
 class QtifwTests < Test::Unit::TestCase
     
-    Test::Unit.at_start do
-        puts 'START: runs only once at start'
-    end
+  Test::Unit.at_start do
+    puts 'START: runs only once at start'
+  end
 
-    Test::Unit.at_exit do
-        puts 'END: runs only once at end'
-    end
+  Test::Unit.at_exit do
+    puts 'END: runs only once at end'
+  end
 
-    def setup
-        puts "Runs before each test method"
-    end
+  def setup
+    puts 'Runs before each test method'
+  end
 
-    def teardown
-        puts "Runs after each test method"
-    end    
+  def teardown
+    puts 'Runs after each test method'
+  end    
 
-    def test_default()
-        assert_equal(2,2)
-    end
+  def test_default()
+    assert_equal(2,2)
+  end
 end
