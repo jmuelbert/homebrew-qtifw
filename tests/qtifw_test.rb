af9f45ee4abb1f4c3253aab74ad7ab5eb3ce94d5
Jürgen Mülbert <juergen.muelbert@gmail.com>
2023-04-15 08:15:20 +0000

    Add test

---
+++ tests/qtifw_test.rb
@@ -0,0 +1,26 @@
+require 'test/unit'
+require_relative '../Formula/qtifw'
+
+class qtifwTests < Test::Unit::TestCase
+    
+    Test::Unit.at_start do
+      puts 'START: runs only once at start'
+    end
+
+    Test::Unit.at_exit do
+      puts 'END: runs only once at end'
+    end
+
+    def setup
+        puts "Runs before each test method"
+    end
+
+    def teardown
+        puts "Runs after each test method"
+    end    
+
+    def test_default()
+
+        assert_equal(2,2))
+    end
+end
