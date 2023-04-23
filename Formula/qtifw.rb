f9fd7ec56edfebee0541b4645b600caacc035b0f
Jürgen <juergen.muelbert@gmail.com>
2023-04-23 12:51:43 +0200

    fix: brew audit issue change dependency orders.

--- Formula/qtifw.rb
+++ Formula/qtifw.rb
@@ -17,8 +17,8 @@ class Qtifw < Formula
 
   head "https://code.qt.io/cgit/installer-framework/installer-framework.git/"
 
-  depends_on "xz"
   depends_on "qt@5"
+  depends_on "xz"
 
   tool_version = "4.5.1"
 
