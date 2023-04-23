248fa5276d3cf619d6a23338f3deca235f0d6643
Jürgen <juergen.muelbert@gmail.com>
2023-04-23 12:33:43 +0200

    Resolve ruby issues. remove standard ruby configs.

--- Formula/qtifw.rb
+++ Formula/qtifw.rb
@@ -10,20 +10,20 @@
 # with homebrew
 class Qtifw < Formula
   desc "The Qt Installer Framework"
-  homepage 'https://doc.qt.io/qtinstallerframework/'
-  url 'https://download.qt.io/official_releases/qt-installer-framework/4.5.1/installer-framework-opensource-src-4.5.1.tar.xz'
-  sha256 '602417a0a2ada5cada8f5b6ad5a160390198b68b46a3721022b7370a971b040a' # DevSkim: ignore DS173237
+  homepage "https://doc.qt.io/qtinstallerframework/"
+  url "https://download.qt.io/official_releases/qt-installer-framework/4.5.1/installer-framework-opensource-src-4.5.1.tar.xz"
+  sha256 "602417a0a2ada5cada8f5b6ad5a160390198b68b46a3721022b7370a971b040a" # DevSkim: ignore DS173237
   version_scheme 1
-  
-  head 'https://code.qt.io/cgit/installer-framework/installer-framework.git/'
 
-  depends_on 'xz'
-  depends_on 'qt@5'
+  head "https://code.qt.io/cgit/installer-framework/installer-framework.git/"
 
-  tool_version = '4.5.1'
-  
+  depends_on "xz"
+  depends_on "qt@5"
+
+  tool_version = "4.5.1"
+
   def install
-    Dir.glob('**/*.pro') do |f|
+    Dir.glob("**/*.pro") do |f|
       inreplace_qt_ifw_pro(f)
     end
 
@@ -32,17 +32,17 @@ class Qtifw < Formula
 
   def inreplace_qt_ifw_pro(file_path)
     inreplace file_path do |s|
-      s.gsub!('$$[QT_INSTALL_LIBS]', '$${PREFIX}/lib', false)
-      s.gsub!('$$[QT_INSTALL_BINS]', '$${PREFIX}/bin', false)
+      s.gsub!("$$[QT_INSTALL_LIBS]", "$${PREFIX}/lib", false)
+      s.gsub!("$$[QT_INSTALL_BINS]", "$${PREFIX}/bin", false)
     end
   end
 
   def build_and_install_qt_ifw
     args = qt_ifw_build_args
-    mkdir 'build' do
-      qt5 = Formula['qt@5'].opt_prefix
-      system "#{qt5}/bin/qmake", *args, '../installerfw.pro'
-      system 'make', 'install'
+    mkdir "build" do
+      qt5 = Formula["qt@5"].opt_prefix
+      system "#{qt5}/bin/qmake", *args, "../installerfw.pro"
+      system "make", "install"
     end
   end
 
@@ -53,7 +53,7 @@ class Qtifw < Formula
       CONFIG+=release
       CONFIG+=libarchive
       CONFIG+=c++11
-      INCLUDEPATH+=#{Formula['xz'].opt_prefix}/include
+      INCLUDEPATH+=#{Formula["xz"].opt_prefix}/include
     ]
   end
 
--- test/qtifw_test.rb
+++ test/qtifw_test.rb
@@ -6,26 +6,26 @@
 # frozen_string_literal: true
 
 # require 'test/unit'
-require_relative '../Formula/qtifw'
+require_relative "../Formula/qtifw"
 
 # Tests for Class Qtifw to install
 # the QT Installer Framework
 # with homebrew
 class QtifwTests < Test::Unit::TestCase
   Test::Unit.at_start do
-    puts 'START: runs only once at start'
+    puts "START: runs only once at start"
   end
 
   Test::Unit.at_exit do
-    puts 'END: runs only once at end'
+    puts "END: runs only once at end"
   end
 
   def setup
-    puts 'Runs before each test method'
+    puts "Runs before each test method"
   end
 
   def teardown
-    puts 'Runs after each test method'
+    puts "Runs after each test method"
   end
 
   def test_default
